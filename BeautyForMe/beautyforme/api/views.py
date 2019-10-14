ecb9d93b351faa3d10366ad06a9bab8e1e54a274
YouuInKeun <keun0390@naver.com>
2019-10-14 23:59:59 +0900

    [ADD} 7주차 발표자료 및 제안서

--- BeautyForMe/beautyforme/api/views.py
+++ BeautyForMe/beautyforme/api/views.py
@@ -22,12 +22,9 @@ class ProfileInfo(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # def post(self, request, format=None):
-    #     serializer = ProfileSerializer(data=request.data)
-    #     print(serializer)
-    #     print(serializer.is_valid())
-    #     print(serializer.errors)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, format=None):
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

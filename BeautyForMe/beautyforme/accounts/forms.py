b311c4961de2dd914778acd6105c84c776c146f4
YouuInKeun <keun0390@naver.com>
2019-10-27 23:23:19 +0900

    [DELETE] 회원가입 폼 이메일 정보 제거

--- BeautyForMe/beautyforme/accounts/forms.py
+++ BeautyForMe/beautyforme/accounts/forms.py
@@ -3,15 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ("username", "password1", "password2")
 
     def save(self, commit=True):
         user = super(CreateUserForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
         if commit:
             user.save()
         return user

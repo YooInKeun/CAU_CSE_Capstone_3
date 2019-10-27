d0761b167b9a94dd9c4d48936e729780edbca99d
YouuInKeun <keun0390@naver.com>
2019-10-27 20:39:16 +0900

    [UPDATE] 프로필 생성 코드 제거

--- BeautyForMe/beautyforme/api/views.py
+++ BeautyForMe/beautyforme/api/views.py
@@ -21,4 +21,17 @@ class ProfileInfo(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+<<<<<<< HEAD
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+=======
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    '''
+    def post(self, request, format=None):
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    '''
+>>>>>>> [UPDATE] 프로필 생성 코드 제거

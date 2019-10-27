a0999ca3fe04137ff76246a610b936b6a50b715f
YouuInKeun <keun0390@naver.com>
2019-10-28 02:58:37 +0900

    [ADD] 프로필 form(추후 수정)

--- BeautyForMe/beautyforme/accounts/forms.py
+++ BeautyForMe/beautyforme/accounts/forms.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from .models import Profile
 
 class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ("username", "password1", "password2")
+        fields = ("username", "password1", "password2", "email", "last_name")
 
     def save(self, commit=True):
         user = super(CreateUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
         if commit:
             user.save()
         return user
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("nickname", "phone", "zip_code", "address", "address_detail")

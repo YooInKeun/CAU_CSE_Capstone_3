42929e6e424a193114f0bfd9d13404122e7ab9e2
YouuInKeun <keun0390@naver.com>
2019-10-28 02:56:38 +0900

    [DELETE] Api Profile Post

--- BeautyForMe/beautyforme/api/views.py
+++ BeautyForMe/beautyforme/api/views.py
@@ -22,10 +22,3 @@ class ProfileInfo(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def post(self, request, format=None):
-        serializer = ProfileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

8ccb58fbceb8d3fb5ae00547f32e131bf2636081
YouuInKeun <keun0390@naver.com>
2019-10-29 04:28:02 +0900

    [ADD] 중간 데모 배포용

--- BeautyForMe/beautyforme/cosmetic/models.py
+++ BeautyForMe/beautyforme/cosmetic/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from jsonfield import JSONField
 
 class Cosmetic(models.Model):
-    test = models.CharField(max_length=200)
+    big_category = models.CharField(max_length=100, default="")
+    small_category = models.CharField(max_length=100, default="")
+    cosmetic_name = models.CharField(max_length=30, default="")
+    brand_name = models.CharField(max_length=30, default="")
+    image_link = models.CharField(max_length=100, default="")
+    option_names = JSONField(default="")
+    tag_names = JSONField(default="")
+
+    def __str__(self):
+        full_name = '[' + self.brand_name + '] '+ self.cosmetic_name
+        return full_name

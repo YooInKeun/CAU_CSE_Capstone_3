d4ff1130202e1ccd6bc28e9951e2d0575fb92ee1
YouuInKeun <keun0390@naver.com>
2019-10-31 21:53:52 +0900

    [ADD] cosmetic admin 표시

--- BeautyForMe/beautyforme/cosmetic/admin.py
+++ BeautyForMe/beautyforme/cosmetic/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Cosmetic
+from .models import *
 
+admin.site.register(Brand)
+admin.site.register(Big_Category)
+admin.site.register(Small_Category)
+admin.site.register(Product)
 admin.site.register(Cosmetic)
+admin.site.register(User_Cosmetic)
+admin.site.register(Cosmetic_Importance)

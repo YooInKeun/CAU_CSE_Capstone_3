a0a0ddd7fdd3ecf85eed4e6e41d5a52a4d30f72b
YouuInKeun <keun0390@naver.com>
2019-11-01 22:20:18 +0900

    [ADD] 브랜드 DB insert

--- BeautyForMe/beautyforme/cosmetic/models.py
+++ BeautyForMe/beautyforme/cosmetic/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 # 브랜드
 class Brand(models.Model):
     brand_name = models.CharField(max_length=100, default="")
+    
+    def __str__(self):
+        return self.brand_name
 
 # 대분류
 class Big_Category(models.Model):
---
+++ BeautyForMe/beautyforme/insert_glowpick_cosmetic_brand.py
@@ -0,0 +1,19 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beautyforme.settings")
+import django
+django.setup()
+from cosmetic.models import *
+import csv
+
+if __name__=='__main__':
+    with open('Sample Data/(전체) 기초.csv') as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        data=list(csv.reader(csvDataFile))
+
+        for i in range(0, len(data), 6):
+            try:
+                Brand.objects.get(brand_name=data[i][0])
+                print("이미 존재하는 브랜드명입니다.")
+            except:
+                Brand(brand_name=data[i][0]).save()
+                print(data[i][0])

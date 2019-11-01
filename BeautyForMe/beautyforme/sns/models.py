4fe2a9f6a99635c96f1afacb778d6fa48674104d
JeongBin <kjh3095000@gmail.com>
2019-11-02 00:02:22 +0900

    [ADD] model 설계

--- BeautyForMe/beautyforme/sns/models.py
+++ BeautyForMe/beautyforme/sns/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class sns(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user')
+    text = models.TextField(blank=True)
+    image = models.ImageField(upload_to='timeline_photo/%Y/%m/%d')
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "text :" + self.text
+
+    class Meta:
+        ordering = ['-created']

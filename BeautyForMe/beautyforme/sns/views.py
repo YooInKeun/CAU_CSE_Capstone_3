d92edd69ba1c60850f90a2261c01c6efe35e7742
JeongBin <kjh3095000@gmail.com>
2019-11-02 00:56:26 +0900

    [ADD] views 설계

--- BeautyForMe/beautyforme/sns/views.py
+++ BeautyForMe/beautyforme/sns/views.py
@@ -1,6 +1,40 @@
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 
 from .models import *
 
+
 class SnsIndexTV(TemplateView):
     template_name = 'sns/index.html'
+
+
+class SnsList(ListView):
+    model = sns
+    template_name_suffix = '_list'
+
+
+class SnsCreate(CreateView):
+    model = sns
+    fields = ['text', 'image']
+    template_name_suffix = '_create'
+    success_url = 'sns/'
+
+
+class SnsUpdate(UpdateView):
+    model = sns
+    fields = ['text', 'image']
+    template_name_suffix = '_update'
+    success_url = 'sns/'
+
+
+class SnsDelete(DeleteView):
+    model = sns
+    template_name_suffix = '_delete'
+    success_url = 'sns/'
+
+class SnsDetail(DetailView):
+    model = sns
+    template_name_suffix = '_detail'
+

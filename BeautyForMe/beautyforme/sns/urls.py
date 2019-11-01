5da4ed8bc5ec33637af6af5f03d0bb7a8961cb57
JeongBin <kjh3095000@gmail.com>
2019-11-02 01:13:01 +0900

    [ADD] url 연결

--- BeautyForMe/beautyforme/sns/urls.py
+++ BeautyForMe/beautyforme/sns/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 from . import views
-
+from .views import SnsCreate, SnsDelete, SnsDetail, SnsList, SnsUpdate
 
 app_name = 'sns'
 urlpatterns = [
+    path('create/', SnsCreate.as_view(), name='create'),
+    path('delete/<int:pk>/', SnsDelete.as_view(), name='delete'),
+    path('update/<int:pk>/', SnsUpdate.as_view(), name='update'),
+    path('detail/<int:pk>/', SnsDetail.as_view(), name='detail'),
+    path('index/sns', SnsList.as_view(), name='list'),
     path('index/', views.SnsIndexTV.as_view(), name='index'),
 ]

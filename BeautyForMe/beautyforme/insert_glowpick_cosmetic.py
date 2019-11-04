1a901063058d7f80d362ecb85240b6051c040368
YouuInKeun <keun0390@naver.com>
2019-11-04 22:19:26 +0900

    [UPDATE] 기초 화장품 DB Insert(태그 insert error 해결 필요)

--- BeautyForMe/beautyforme/insert_glowpick_cosmetic.py
+++ BeautyForMe/beautyforme/insert_glowpick_cosmetic.py
@@ -6,24 +6,30 @@ from cosmetic.models import *
 import csv
 
 if __name__=='__main__':
-    with open('db_insert_data/All Data/베이스.csv') as csvDataFile:
+    with open('db_insert_data/All Data/기초.csv') as csvDataFile:
         csvReader = csv.reader(csvDataFile)
         data=list(csv.reader(csvDataFile))
+
         for i in range(0, len(data), 6):
-            try:
-                for j in range(len(data[i+2])):
+            for j in range(len(data[i+2])):
+                try:
                     Cosmetic.object.get(product = Product.objects.get(brand=Brand.objects.get(brand_name=data[i][0]), product_name=data[i+1][0]),
                                         type_name = data[i+2][j])
                     print(data[i+1][0])
                     print("이미 존재하는 제품입니다.")
-            except:
-                try:
-                    for j in range(len(data[i+2])):
-                        cosmetic = Cosmetic(product = Product.objects.get(brand=Brand.objects.get(brand_name=data[i][0]), product_name=data[i+1][0]),
+                except:
+                    try:
+                        Cosmetic.object.get(product = Product.objects.get(brand=Brand.objects.get(brand_name=data[i][0]), product_name=data[i+1][0]),
                                             type_name = data[i+2][j],                       
                                             rgb_value = "None", 
                                             image_link = data[i+4][0])
-                        cosmetic.save()
-                except:
-                    print(data[i+1][0])
-                    print("error")
+                        print("이미 존재하는 제품입니다.")
+                    except:
+                        try:
+                            cosmetic = Cosmetic(product = Product.objects.get(brand=Brand.objects.get(brand_name=data[i][0]), product_name=data[i+1][0]),
+                                                type_name = "None",                       
+                                                rgb_value = "None", 
+                                                image_link = data[i+4][0])
+                            cosmetic.save()
+                        except:
+                            print("error")

b3e17ffea462e6dd921ead51b6eedf0e328d52b1
YouuInKeun <keun0390@naver.com>
2019-11-15 04:17:11 +0900

    [UPDATE] 대체 화장품 insert

--- BeautyForMe/beautyforme/insert_similar_cosmetics.py
+++ BeautyForMe/beautyforme/insert_similar_cosmetics.py
@@ -8,6 +8,8 @@ django.setup()
 from cosmetic.models import *
 
 if __name__=='__main__':
+
+    # print(Cosmetic.objects.get(id=448).similar_cosmetics)
     # test = {}
     # for i in range(3):
     #     test[i] = "테스트"
@@ -17,17 +19,22 @@ if __name__=='__main__':
     #     cosmetic.similar_cosmetics = {}
     #     cosmetic.save()
 
-    for cosmetic in Cosmetic.objects.all():
+    # for cosmetic in Cosmetic.objects.all():
     #     print(cosmetic.similar_cosmetics)
+
+    for cosmetic in Cosmetic.objects.all():
+        # print(cosmetic.similar_cosmetics)
 
         # print(cosmetic.product.category)
         i=0
-        for compared_cosmetic in Cosmetic.objects.all():
+        tmp = {}
+        print(str(cosmetic.id) + ' 시작!')
+        compared_cosmetics = Cosmetic.objects.filter(product__category=cosmetic.product.category)
+        for compared_cosmetic in compared_cosmetics:
             if cosmetic.rgb_value != "None":
                 if cosmetic.rgb_value == compared_cosmetic.rgb_value and cosmetic.product.category == compared_cosmetic.product.category and cosmetic != compared_cosmetic:
-                    tmp = {}
                     tmp[i] = compared_cosmetic.id
-                    print(str(cosmetic) + ' (id값:' + str(cosmetic.id) + ') / 유사화장품: ' + str(compared_cosmetic) + ' (id값:' + str(compared_cosmetic.id) + ')')
+                    print(str(cosmetic) + ', id값: ' + str(cosmetic.id) + ', rgb: ' + str(cosmetic.rgb_value) + '// 유사화장품: ' + str(compared_cosmetic) + ', id값: ' + str(compared_cosmetic.id) + ', rgb:' + str(compared_cosmetic.rgb_value))
                     i += 1
         cosmetic.similar_cosmetics = tmp
         cosmetic.save()

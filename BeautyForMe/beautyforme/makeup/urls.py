8b151dcb35a57178e0a74c081bf3f553f1db739a
bumsu <bumsu0211@gmail.com>
2019-11-30 02:11:59 +0900

    조합 결과 페이지 url 에러 발생

--- BeautyForMe/beautyforme/makeup/urls.py
+++ BeautyForMe/beautyforme/makeup/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('recommend/', views.RecommendMakeupTV.as_view(), name='recommend'),
     path('recommend/result/', views.recommend_result, name='recommend_result'),
     path('recommend/result/video/', views.VideoTV.as_view(), name='video'),
-    path('recommend/result/video/detail/<int:pk>/', views.VideoDetailTV.as_view(), name='vidio_detail'),
+    path('recommend/result/video/detail/<int:pk>/', views.VideoDetailTV.as_view(), name='video_detail'),
 ]

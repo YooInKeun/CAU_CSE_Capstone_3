63e223ea99cd4043418e780be1159dab49d85a5a
YouuInKeun <keun0390@naver.com>
2019-11-29 09:18:37 +0900

    [ADD] 동영상 추천 기능 완료

--- BeautyForMe/beautyforme/makeup/views.py
+++ BeautyForMe/beautyforme/makeup/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-
+from django.core import serializers
 from .models import *
+from cosmetic.models import *
 import json
 
 class RecommendMakeupTV(TemplateView):
@@ -11,14 +12,75 @@ def recommend_result(request):
     raws = json.loads(request.POST.get('selected_cosmetics'))
     cosmetics = {}
     cosmetics['cosmetics'] = raws
-    
-    cosmetic_ids = []
+    selected_cosmetic_ids = []
     for raw in raws:
-        cosmetic_ids.append(raw['id'])
+        selected_cosmetic_ids.append(raw['id'])
+
+    videos = Video.objects.all()
+    video_scores = {}
+    video_scores_contained_cosmetic_ids = []
+    for video in videos:
+        video_cosmetic_ids = []
+        contained_cosmetic_ids = []
+        raw = video.cosmetics
+        raw = raw.replace("'", "\"")
+        json_video_cosmetic_ids = json.loads(raw)
+        for key in json_video_cosmetic_ids.keys():
+            video_cosmetic_ids.append(int(json_video_cosmetic_ids[key]))
+
+        score = 0
+        for selected_cosmetic_id in selected_cosmetic_ids:
+            for video_cosmetic_id in video_cosmetic_ids:
+                if selected_cosmetic_id == video_cosmetic_id:
+                    big_category = str(Cosmetic.objects.get(id=selected_cosmetic_id).product.category.big_category)
+                    small_category = str(Cosmetic.objects.get(id=selected_cosmetic_id).product.category)
+                    cosmetic_importance = json.loads(Cosmetic_Importance.objects.get(user=request.user).importance)
+                    importance = Cosmetic_Importance.objects.get(user=request.user)
+                    contained_cosmetic_ids.append(selected_cosmetic_id)
+                    parenthesis_pos = small_category.find(']')
+                    small_category = small_category[parenthesis_pos+2:]
+
+                    if small_category == '아이브로우-마스카라&리퀴드' or small_category == '아이브로우-파우더' or small_category == '아이브로우-펜슬':
+                        small_category = '아이브로우'
+                    elif small_category == '아이라이너-리퀴드' or small_category == '아이라이너-펜슬&젤':
+                        small_category = '아이라이너'
+                    score += float(cosmetic_importance[big_category][small_category])
+        if score != 0:
+            video_scores[video.id] = score
+            video_scores_contained_cosmetic_ids.append([score, contained_cosmetic_ids])
     
-    print(cosmetics)
-    print(cosmetic_ids)
-    return render(request, 'makeup/recommend_result.html')
+    video_queryset = Video.objects.filter(title="Fuck You")
+    context = {}
+    sorted_videos = sorted(video_scores.items(), key=lambda element:element[1], reverse=True)
+    for sorted_video in sorted_videos:
+        video_queryset |= Video.objects.filter(pk=sorted_video[0])
+    context['cosmetics'] = cosmetics['cosmetics']
+    context['videos'] = []
+    video_list = json.loads(serializers.serialize('json', video_queryset))
+    for video in video_list:
+        context['videos'].append(video['fields'])
+    for idx, val in enumerate(sorted_videos):
+        context['videos'][idx]['score'] = val[1]
+    
+    contained_cosmetics = []
+    sorted_scores_cosmetic_ids = sorted(video_scores_contained_cosmetic_ids, key=lambda element:element[0], reverse=True)
+    for idx, sorted_scores_cosmetic_id in enumerate(sorted_scores_cosmetic_ids):
+        context['videos'][idx]['containedCosmetics'] = []
+        cosmetic_queryset = Cosmetic.objects.filter(rgb_value="Fuck You")
+        for cosmetic_id in sorted_scores_cosmetic_id[1]:
+            cosmetic_queryset |= Cosmetic.objects.filter(pk=cosmetic_id)
+        cosmetic_list = json.loads(serializers.serialize('json', cosmetic_queryset))
+        for cosmetic in cosmetic_list:
+            cosmetic_info = {}
+            cosmetic_info['brandName'] = str(Product.objects.get(pk=cosmetic['fields']['product']).brand)
+            product_name = str(Product.objects.get(pk=cosmetic['fields']['product']))
+            parenthesis_pos = product_name.find(']')
+            product_name = product_name[parenthesis_pos+2:]
+            cosmetic_info['productName'] = product_name
+            cosmetic_info['typeName'] = cosmetic['fields']['type_name'].strip()
+            context['videos'][idx]['containedCosmetics'].append(cosmetic_info) 
+    print(context)
+    return render(request, 'makeup/recommend_result.html', {'context':context})
 
 # class RecommendResultTV(TemplateView):
 #     template_name = 'makeup/recommend_result.html'
